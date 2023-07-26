9820b5d5ece7c46bb72993fa13a78862216bb3d3
chialia <barbiemail01@gmail.com>
2023-07-26 17:13:19 +0800

    Add Azure Speech to text Method
    新增利用微軟的語音轉文字

--- vs Web/Run.py
+++ vs Web/Run.py
@@ -4,6 +4,7 @@ import openai
 from pydub import AudioSegment
 import speech_recognition as speechrec
 from gensim.summarization import summarize
+import azure.cognitiveservices.speech as speechsdk
 from datetime import date
 import time
 import pyaudio
@@ -11,6 +12,16 @@ import pyaudio
 # global wav_file
 
 app = Flask(__name__)
+
+# 設置open api 端點
+openai.api_type = "azure"
+openai.api_base = "https://cshitinternopenai.openai.azure.com/"
+openai.api_version = "2023-03-15-preview"
+openai.api_key = "0be4adcd512d4b09b7e44d50325f4bf9"
+
+SPEECH_KEY = "c9d3e6d440214af3bc175d4c31809a44"
+SPEECH_REGION = "eastasia"
+
 
 @app.route('/')
 def Hello():
@@ -36,12 +47,7 @@ def upload():
         if request.values['send']=='提交':
             data1=request.values['data']
             # 錄音轉文字 利用Speech Recognition recognize_whisper
-            # 設置open api 端點
-            openai.api_type = "azure"
-            openai.api_base = "https://cshitinternopenai.openai.azure.com/"
-            openai.api_version = "2023-03-15-preview"
-            openai.api_key = "0be4adcd512d4b09b7e44d50325f4bf9"
-
+           
             sperec = speechrec.Recognizer()
 
             #假設file的檔案
@@ -147,11 +153,6 @@ def micro():
             except speechrec.UnknownValueError:
                 text = "無法翻譯"
             txt = text
-            openai.api_type = "azure"
-            openai.api_base = "https://cshitinternopenai.openai.azure.com/"
-            openai.api_version = "2023-03-15-preview"
-            openai.api_key = "0be4adcd512d4b09b7e44d50325f4bf9"
-
 
             # 讀取今天的日期
             today = date.today()
@@ -181,6 +182,88 @@ def micro():
             # return render_template('micro.html',text=response['choices'][0]['message']['content']) #用在第二種方法 回傳text值，他是對應前端的{{text}}屬性
             return jsonify({'answer' : response['choices'][0]['message']['content']}) #用在第一種方法 回傳json格式
     return render_template('micro.html',text="")
+
+# Azure speech_to_text 方法
+@app.route('/speech_to_text', methods=['POST','GET']) #錄音檔語音摘要
+# 如果要使用/speech_to_text 就把 ajax裡的 url改掉
+# 錄音檔轉文字
+def speech_to_text():
+    if request.method =='POST':
+        if request.values['send']=='提交':
+            data1=request.values['data']
+            First_Name= data1.split(".")[0]
+
+            weatherfilename = 'D:\\中山醫實習\\專案\\Practice\\video\\'+data1
+
+            speech_config = speechsdk.SpeechConfig(SPEECH_KEY,SPEECH_REGION)
+            speech_config.speech_recognition_language="zh-Tw"   #"zh-Tw" 
+            audio_config = speechsdk.audio.AudioConfig(filename=weatherfilename)
+            speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
+
+            done=False
+
+            def stop_cb(evt):
+                # print('CLOSING on {}'.format(evt))
+                speech_recognizer.stop_continuous_recognition()
+                nonlocal done
+                done = True
+            
+            all_results = []
+            def handle_final_result(evt):
+                all_results.append(evt.result.text) #讀出evt text裡的字串
+            print(all_results)
+            speech_recognizer.recognized.connect(handle_final_result)
+
+            # speech_recognizer.recognizing.connect(lambda evt: print('{} '.format(evt)))
+            # speech_recognizer.recognized.connect(lambda evt: print('RECOGNIZED: {}'.format(evt)))
+            # speech_recognizer.session_started.connect(lambda evt: print('SESSION STARTED: {}'.format(evt)))
+            # speech_recognizer.session_stopped.connect(lambda evt: print('SESSION STOPPED {}'.format(evt)))
+            # speech_recognizer.canceled.connect(lambda evt: print('CANCELED {}'.format(evt)))
+
+            speech_recognizer.session_stopped.connect(stop_cb)
+            speech_recognizer.canceled.connect(stop_cb)
+
+            speech_recognizer.start_continuous_recognition()
+            while not done:
+                time.sleep(.5)
+            
+            all_results = "".join(all_results)
+            print(all_results)
+            # return all_results
+            # return render_template('index.html',text=all_results)
+
+                        #將檔案放入的路徑為
+            ffpath = "txt\\"
+
+            # 讀取今天的日期
+            today = date.today()
+            # 將日期變成字串
+            todayStr = today.strftime("%Y-%m-%d")
+
+            #判斷是否已經存在了
+            if not os.path.exists(ffpath):
+                os.makedirs(ffpath)
+            ffname = os.path.join(ffpath, '文字檔'+First_Name+todayStr+".txt") #將輸出的資料命名為 "output.txt"
+
+            with open(ffname,"w",encoding='UTF-8') as file: #寫入檔案到資料夾中
+                file.write(all_results)
+
+            print("已儲存") #成功就會print
+
+            response = openai.ChatCompletion.create(
+            engine="CSHITIntern", # engine = "deployment_name".
+            messages=[
+                {"role": "system", "content": "我是一個秘書要做會議紀錄"}, #人物設定
+                {"role": "assistant", "content": all_results},#文章
+                {"role": "user", "content": "可以幫我們統整重點" }#想要叫GPT做的事
+                ])
+
+            ffname = os.path.join(ffpath,'摘要檔'+First_Name+todayStr+'.txt') #將輸出的資料命名
+            with open(ffname,"w",encoding='UTF-8') as file: #寫入檔案到資料夾中
+                file.write(response['choices'][0]['message']['content'])
+            # return render_template('index.html',text=response['choices'][0]['message']['content'])
+            return jsonify({'answer' : response['choices'][0]['message']['content']}) # 回傳json格式
+    return render_template('index.html',text="")
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0',port='4000',debug=True)
