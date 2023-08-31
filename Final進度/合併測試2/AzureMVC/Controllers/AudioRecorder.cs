f8dc2346f23a0d0bac731c47751a660763aec14e
Pau1l24 <138411782+Pau1l24@users.noreply.github.com>
2023-08-31 14:16:08 +0800

    final
    合併2

---
+++ Final進度/合併測試2/AzureMVC/Controllers/AudioRecorder.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CognitiveServices.Speech.Audio;
+using System;
+
+namespace AzureMVC.Controllers
+{
+    internal class AudioRecorder
+    {
+        private string outputPath;
+        private AudioStreamFormat audioFormat;
+
+        public AudioRecorder(string outputPath, AudioStreamFormat audioFormat)
+        {
+            this.outputPath = outputPath;
+            this.audioFormat = audioFormat;
+        }
+    }
+}
---
+++ Final進度/合併測試2/AzureMVC/Models/FileModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AzureMVC.Models
+{
+    public class FileModel
+    {
+        [Required]
+        [Display(Name="檔名")]
+        public string Filename { get; set; }
+    }
+}
