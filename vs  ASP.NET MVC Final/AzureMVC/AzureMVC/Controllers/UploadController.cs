532ff9eb3c233bdf904476d6e1f9ac04c6f55c2c
chialia <barbiemail01@gmail.com>
2023-08-31 08:30:31 +0800

    Update

--- vs  ASP.NET MVC Final/AzureMVC/AzureMVC/Controllers/UploadController.cs
+++ vs  ASP.NET MVC Final/AzureMVC/AzureMVC/Controllers/UploadController.cs
@@ -26,8 +26,10 @@ namespace AzureMVC.Controllers
 
         //static string speechKey = Environment.GetEnvironmentVariable("SPEECH_KEY");//這裡因為我在cmd裡把SPEECH_KEY設為c9d3e6d440214af3bc175d4c31809a44
         //static string speechRegion = Environment.GetEnvironmentVariable("SPEECH_REGION");//SPEECH_REGION為eastasia
+        //語音檔轉文字用的
         static string speechKey = "c9d3e6d440214af3bc175d4c31809a44";
         static string speechRegion = "eastasia";
+        //多人會議轉譯用的
         static string subscriptionKey = "2581928f7d5042e190f8fb24c94540a1";
         static string serviceRegion = "eastus";
 
@@ -39,25 +41,19 @@ namespace AzureMVC.Controllers
         static StringBuilder MicroResult = new StringBuilder();
         private WaveInEvent waveInEvent;
         private WaveFileWriter waveFileWriter;
+        static string MicroText;
+        private bool isRecognizing = false;
+        private AudioConfig audioConfig;
+        private SpeechConfig speechConfig, config;
 
-
-        //coversation speaker recognize
+        //coversation speaker recognize多人會議轉譯
         private ConversationTranscriber conversationTranscriber;
-
-        static StringBuilder all_result = new StringBuilder();
 
         //TranscribeSpeeching()、TranscribeSpeechFinal()
         static StringBuilder TrascribeTxt = new StringBuilder(); //摘要(包涵 切割文字檔後跑的、直接轉完文字跑的)
         static StringBuilder CognizeText = new StringBuilder();
-        static string MicroText;
-        private bool isRecognizing = false;
-        private SpeechConfig speechConfig, config;
-        private AudioConfig audioConfig;
-        private Task recTask;
-        private static FileStream audioOutputStream;
 
-
-        //將變數初始化
+        //將變數初始化(多人轉譯)
         public UploadController()
         {
             //單人辨識的初始化宣告(用於麥克風)
@@ -65,7 +61,7 @@ namespace AzureMVC.Controllers
             speechConfig.SpeechRecognitionLanguage = "zh-tw";
             audioConfig = AudioConfig.FromDefaultMicrophoneInput();
             speechRecognizers = new SpeechRecognizer(speechConfig, audioConfig);
-            //多人辨識的初始化宣告(用於麥克風)
+            //多人辨識的初始化宣告(用於麥克風)(這個還不行用，因為麥克風現在還未支援單聲道的)
             config = SpeechConfig.FromSubscription(subscriptionKey, serviceRegion);
             config.SpeechRecognitionLanguage = "zh-tw";
             config.SetProperty("ConversationTranscriptionInRoomAndOnline", "true");
@@ -79,19 +75,12 @@ namespace AzureMVC.Controllers
             return View();
         }
 
-////------------------------------------------------------------------------------------------------------------        
+////語音檔轉文字------------------------------------------------------------------------------------------------------------        
 
         ////語音檔辨識 上傳錄音檔的介面
         [HttpGet]
         public async Task<ActionResult> UploadFile()
         {
-            //刪除資料夾檔案
-            //DirectoryInfo FP = new DirectoryInfo(Server.MapPath("~/UploadedFiles/"));//抓出檔案路徑
-            //FileInfo[] files = FP.GetFiles();//抓取檔案
-            //foreach (FileInfo file in files)
-            //{
-            //    file.Delete();
-            //}
             //讀取資料夾
             string[] fpaths = Directory.GetFiles(Server.MapPath("~/UploadedFiles/"));
             List<FileModel> files = new List<FileModel>();
@@ -318,14 +307,14 @@ namespace AzureMVC.Controllers
                 //await stopRecogintion.Task;
                 //每個檔案變識幾秒或是分鐘.....
                 await Task.Delay(TimeSpan.FromSeconds(durationInSeconds+20));//這裡設定在執行時，會跑到60分鐘才會結束
-                                                          //await Task.WhenAny(stopRecogintion.Task);
-                                                          //等待變識結束
+                //await Task.WhenAny(stopRecogintion.Task);
+                //等待變識結束
                 await speechRecognizer.StopContinuousRecognitionAsync();
                 TextResult.Append("@");
             }
         }
 
-        ////------------------------------------------------------------------------------------------------------------        
+////麥克風轉錄------------------------------------------------------------------------------------------------------------        
 
         ////即時錄音辨識 即時錄音介面
         [HttpGet]
@@ -572,68 +561,7 @@ namespace AzureMVC.Controllers
             return View();
         }
 
-
-        //產生會議記錄
-        //[HttpPost]
-        //public async Task<ActionResult> TranscribeSpeechFinal(string text)
-        //{
-
-        //    OpenAIClient client = new(new Uri("https://cshitinterngpt4.openai.azure.com/"), new AzureKeyCredential("71e9950da8c34bc7805520df08984c21"));
-        //    var chatCompletionsOptions = new ChatCompletionsOptions()
-        //    {
-        //        Messages =
-        //        {
-        //            new ChatMessage(ChatRole.System, "紀錄會議紀錄的人"),
-        //            new ChatMessage(ChatRole.User, text),
-        //            //new ChatMessage(ChatRole.User, ,
-        //            new ChatMessage(ChatRole.User, "統整重點並以會議紀錄的型式書寫"),
-        //        },
-        //        //文本的「創造性」程度。這個參數控制了生成文本時模型選擇詞語的隨機程度
-        //        //Temperature = 1,
-        //        //在生成下一個詞語時，只考慮詞語的累積機率分佈中最高的部分，例如:1、0.1
-        //        NucleusSamplingFactor = (float)0.1,
-        //        //存在懲罰，控制包含特定詞語的文本 較小的值會傾向於生成包含輸入提示中提到的詞語或主題的文本，而較大的值會減少這種傾向，0或是1比較適合。               
-        //        PresencePenalty = 1,
-        //        //頻率懲罰，控制重複詞語的頻率，調整生成文本中各詞語的重複頻率，以產生更多多樣性的輸出，會有?產生 
-        //        //FrequencyPenalty = 1,
-        //        // 最大標記數，限制文本長度
-        //        MaxTokens = 3000 //MaxTokens = 32768 這樣在返回的結果中，生成的文本將會有最多32768個標記，這會跟gpt的上限為準
-        //    };
-        //    try
-        //    {
-        //        Response<StreamingChatCompletions> response = await client.GetChatCompletionsStreamingAsync(
-        //                     deploymentOrModelName: "CSHInternGPT4-32K", chatCompletionsOptions);
-
-        //        StreamingChatCompletions streamingChatCompletions = response.Value;
-
-        //        TrascribeTxt.Append("會議重點整理");
-
-        //        await foreach (StreamingChatChoice choice in streamingChatCompletions.GetChoicesStreaming())
-        //        {
-        //            await foreach (ChatMessage message in choice.GetMessageStreaming())
-        //            {
-        //                TrascribeTxt.Append(message.Content);
-        //                //TextResult.Append(message.Content);
-        //                Console.Write(message.Content);
-        //            }
-        //            Console.WriteLine();
-        //        }
-        //        //ViewBag.text = TextResult.ToString();
-        //        string content = TrascribeTxt.ToString();
-        //        //string savePath = @"~\UploadedFiles"; // 替换为您希望保存的路径和文件名
-        //        //WriteStringToWordAndSave(content);
-        //        ViewBag.txtRes = "摘要完成";
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        Console.WriteLine("An error occurred: " + ex.Message);
-        //    }
-        //    return View();
-        //}
-
-
-
-        //輸出word
+        //輸出Word檔
         public void WriteStringToWordAndSave(string content)
         {
             // 創建Word應用程式
@@ -1047,47 +975,7 @@ namespace AzureMVC.Controllers
             return Content(res,"text/plain");
         }
 
-        //[HttpPost]
-        //將單音道或雙聲道---> 八聲道(對話檔一定要轉檔)
-        //public static string CovertTo_Eight_Voice(string FilePath, string FolderPath)
-        //{
-        //    string inputFile = FilePath; // 輸入的雙聲道or單聲道音訊檔案
-        //    string fileName = Path.GetFileNameWithoutExtension(FilePath);
-        //    //輸出的檔案
-        //    string outputFile = Path.Combine(FolderPath, fileName + "_1" + ".wav");
-
-        //    //var outputFile = @"D:\radio\TaipeiMeet35min1.wav"; // 輸出的八聲道音訊檔案
-
-        //    // 使用 FFmpeg 將雙聲道轉換成八聲道
-        //    string ffmpegPath = @"C:\ffmpeg-6.0-full_build\bin\ffmpeg.exe"; // FFmpeg 執行檔路徑
-        //    //string command = $"-i \"{inputFile}\" -ac 8 \"{outputFile}\""; // 轉換指令
-        //    //string command = $"-i \"{inputFile}\" -filter_complex \"[0:a]pan=8c|c0=c0|c1=c0|c2=c0|c3=c0|c4=c0|c5=c0|c6=c0|c7=c0[aout]\" -map \"[aout]\" -ac 8 \"{outputFile}\"";
-        //    //string command = $"-i \"{inputFile}\" -ac 8 -filter_complex \"[0:a]channelmap=channel_layout=octagonal:map=FL[aout]\" -c:a pcm_s16le \"{outputFile}\"";
-        //    //string command = $"-i \"{inputFile}\" -filter_complex \"[0:a]channelmap=channel_layout=octagonal:map=FL|FL|FL|FL|FL|FL|FL|FL[aout]\" -c:a pcm_s16le \"{outputFile}\"";
-        //    string command = $"-i \"{inputFile}\" -ac 8 -filter_complex \"[0:a]aevalsrc=0:d=0.0[s0];[0:a][s0]amerge=inputs=2[aout]\" -map \"[aout]\" -c:a pcm_s16le \"{outputFile}\"";
-
-        //    ProcessStartInfo processInfo = new ProcessStartInfo(ffmpegPath)
-        //    {
-        //        Arguments = command,
-        //        CreateNoWindow = true,
-        //        RedirectStandardOutput = true,
-        //        RedirectStandardError = true,
-        //        UseShellExecute = false,
-        //    };
-
-        //    Process process = new Process { StartInfo = processInfo };
-        //    process.Start();
-        //    process.WaitForExit();
-
-        //    if (process.ExitCode == 0)
-        //    {
-        //        Console.WriteLine("轉換成功。");
-        //    }
-        //    return outputFile;
-        //}
-
-
-
+        //去除雜音(效果不太好，所以目前沒有使用)
         [HttpPost]
         public static string Denoiser(string fpath,string folderpath)
         {
@@ -1245,7 +1133,9 @@ namespace AzureMVC.Controllers
             }
         }
 
-        ////多人辨識  即時語音變識------------------------------------------------------------------------------------------------------------        
+        ////多人辨識  即時語音變識 (這個現在還不行)------------------------------------------------------------------------------------------------------------        
+        
+        //開始錄音
         [HttpPost]
         public async Task<ActionResult> SpeakerStartRecord()
         {
@@ -1386,6 +1276,7 @@ namespace AzureMVC.Controllers
 
         }
 
+        //結束錄音
         [HttpPost]
         public async Task<ActionResult> SpeakerStopRecord()
         {
@@ -1468,69 +1359,3 @@ namespace AzureMVC.Controllers
 
     }
 }
-//public async Task NameSet()
-//{
-//    var voiceSignatureWaveFileUser1 = @"D:\radio\舒.wav";
-//    var voiceSignatureWaveFileUser2 = @"D:\radio\魏.wav";
-//    //var conversationWaveFile = @"D:\radio\砂源對話3.wav";
-//    // Create voice signature for the user1 and convert it to json string
-//    var voiceSignature = await CreateVoiceSignatureFromVoiceSample(voiceSignatureWaveFileUser1, subscriptionKey, serviceRegion);
-//    //var voiceSignature = CreateVoiceSignatureFromVoiceSample ("Guest1",subscriptionKey, serviceRegion);
-//    voiceSignatureUser1 = JsonConvert.SerializeObject(voiceSignature.Signature);
-
-//    // Create voice signature for the user2 and convert it to json string
-//    voiceSignature = await CreateVoiceSignatureFromVoiceSample(voiceSignatureWaveFileUser2, subscriptionKey, serviceRegion);
-//    //voiceSignature = CreateVoiceSignatureFromVoiceSample("Guest2", subscriptionKey, serviceRegion);
-//    voiceSignatureUser2 = JsonConvert.SerializeObject(voiceSignature.Signature);
-
-//}
-
-//private static async Task<List<string>> GetRecognizerResult(ConversationTranscriber recognizer, string conversationId)
-//{
-//    List<string> recognizedText = new List<string>();
-//    recognizer.Transcribed += (s, e) =>
-//    {
-//        if (e.Result.Text.Length > 0)
-//        {
-//            recognizedText.Add(e.Result.Text);
-//            //Console.WriteLine($"TRANSCRIBED: {e.Result.Text}, {e.Result.ResultId}, {e.Result.Reason}, {e.Result.UserId}, {e.Result.OffsetInTicks}, {e.Result.Duration}");
-//            Console.WriteLine($"{e.Result.UserId}: {e.Result.Text}, Time:{e.Result.Duration}");
-//        }
-
-//    };
-//    await CompleteContinuousRecognition(recognizer, conversationId);
-//    recognizer.Dispose();
-//    return recognizedText;
-//}
-
-//private static async Task CompleteContinuousRecognition(ConversationTranscriber recognizer, string conversationId)
-//{
-//    var stopRecognition = new TaskCompletionSource<int>(TaskCreationOptions.RunContinuationsAsynchronously);
-
-//    TaskCompletionSource<int> taskCompletionSource = new TaskCompletionSource<int>(TaskCreationOptions.RunContinuationsAsynchronously);
-
-//    recognizer.SessionStopped += (s, e) =>
-//    {
-//        taskCompletionSource.TrySetResult(0);
-//    };
-
-//    recognizer.Canceled += (s, e) =>
-//    {
-//        Console.WriteLine($"CANCELED: Reason={e.Reason}");
-//        if (e.Reason == CancellationReason.Error)
-//        {
-//            Console.WriteLine($"CANCELED: ErrorCode={e.ErrorCode}");
-//            Console.WriteLine($"CANCELED: ErrorDetails={e.ErrorDetails}");
-//            Console.WriteLine($"CANCELED: Did you update the subscription info?");
-//            throw new System.ApplicationException("${e.ErrorDetails}");
-//        }
-//        taskCompletionSource.TrySetResult(0);
-//    };
-//    await recognizer.StartTranscribingAsync().ConfigureAwait(false);
-//    //await Task.WhenAny(new[] { stopRecognition.Task });
-//    await Task.Delay(TimeSpan.FromSeconds(300));
-
-//    //await Task.WhenAny(taskCompletionSource.Task);
-//    //await Task.WhenAny(taskCompletionSource.Task, Task.Delay(TimeSpan.FromSeconds(10)));
-//    await recognizer.StopTranscribingAsync().ConfigureAwait(false);
-//}
